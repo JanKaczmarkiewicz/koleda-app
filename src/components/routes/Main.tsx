2e5562f2f561b5b423b9ae02fbf7f355b5e15c05
positiveKiddo@gmail.com <positiveKiddo@gmail.com>
2020-09-11 09:12:29 +0200

    visits list info display

--- src/components/routes/Main.tsx
+++ src/components/routes/Main.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/react-hooks";
 import Container from "../layout/Container";
-import { FlatList, Text, TouchableOpacity } from "react-native";
-import { AcolytePastoralVisits } from "../../generated/AcolytePastoralVisits";
+import { FlatList, TouchableOpacity } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "./LoggedApp";
+import {
+  AcolyteActiveDaysVariables,
+  AcolyteActiveDays,
+} from "../../generated/AcolyteActiveDays";
+import { useAuthContext, useSeasonContext } from "@koleda/common";
+import { Card, List, Paragraph, Surface, Title } from "react-native-paper";
 
-const ACOLYTE_PASTORAL_VISTIS = gql`
-  query AcolytePastoralVisits {
-    pastoralVisits {
+const ACOLYTE_ACTIVE_DAYS = gql`
+  query AcolyteActiveDays($daysInput: DaysInput!, $user: String!) {
+    days(input: $daysInput) {
       id
-      hour
+      visitDate
+      reeceDate
+      pastoralVisits(input: { acolyte: $user }) {
+        priest {
+          id
+          username
+        }
+        acolytes {
+          id
+          username
+        }
+        id
+        hour
+      }
     }
   }
 `;
@@ -22,24 +40,54 @@ type Props = {
 };
 
 const Main: React.FC<Props> = ({ navigation }) => {
-  const { loading, error, data } = useQuery<AcolytePastoralVisits>(
-    ACOLYTE_PASTORAL_VISTIS
-  );
+  const { me } = useAuthContext();
+  const { currentSeason } = useSeasonContext();
+
+  const todaysDate = new Date().toLocaleDateString();
+  const futureDate = new Date("10.12.2030").toISOString();
+
+  const { loading, error, data } = useQuery<
+    AcolyteActiveDays,
+    AcolyteActiveDaysVariables
+  >(ACOLYTE_ACTIVE_DAYS, {
+    variables: {
+      daysInput: {
+        from: todaysDate,
+        to: futureDate,
+        season: currentSeason.id,
+      },
+      user: me!.id,
+    },
+  });
 
   if (loading) return <div>loading...</div>;
-  if (error || !data || !data.pastoralVisits) return <div>error</div>;
+  if (error || !data || !data.days) return <div>error</div>;
+
+  const days = data.days.filter(
+    ({ pastoralVisits }) => pastoralVisits.length > 0
+  );
 
   return (
     <Container>
       <FlatList
-        data={data.pastoralVisits}
-        renderItem={({ item: { id, hour } }) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Visit", { visitId: id })}
-          >
-            <Text>{id}</Text>
-            <Text>{hour}</Text>
-          </TouchableOpacity>
+        data={days}
+        renderItem={({ item: { id, pastoralVisits, visitDate } }) => (
+          <Card key={id}>
+            <Card.Content>
+              <Title>{new Date(visitDate).toLocaleDateString()}</Title>
+              {pastoralVisits.map(({ id, hour, acolytes }) => (
+                <TouchableOpacity key={id}>
+                  <List.Item
+                    description={hour}
+                    title={acolytes.map(({ username }) => username).join(", ")}
+                    onPress={() =>
+                      navigation.navigate("Visit", { visitId: id })
+                    }
+                  />
+                </TouchableOpacity>
+              ))}
+            </Card.Content>
+          </Card>
         )}
       />
     </Container>
--- src/components/routes/StreetEntrancesGroup.tsx
+++ src/components/routes/StreetEntrancesGroup.tsx
@@ -15,37 +15,35 @@ const StreetEntrancesGroup: React.FC<Props> = ({
   entrances,
   onItemPress,
 }) => (
-  <Surface key={streetName}>
-    <List.Section
-      style={{
-        width: "100%",
-        backgroundColor: stringToColour(streetName),
-      }}
-    >
-      <List.Subheader>{streetName}</List.Subheader>
-      {sortByHouseNumber(entrances, ({ house }) => house?.number).map(
-        (entrance) => {
-          const { id, house, reeceState, visitState, comment } = entrance;
-          return (
-            <Fragment key={id}>
-              <Divider />
-              <List.Item
-                onPress={() => onItemPress(entrance)}
-                title={house?.number}
-                description={comment}
-                right={() => (
-                  <>
-                    {renderStateListIcon(reeceState)}
-                    {renderStateListIcon(visitState)}
-                  </>
-                )}
-              />
-            </Fragment>
-          );
-        }
-      )}
-    </List.Section>
-  </Surface>
+  <List.Section
+    style={{
+      width: "100%",
+      backgroundColor: stringToColour(streetName),
+    }}
+  >
+    <List.Subheader>{streetName}</List.Subheader>
+    {sortByHouseNumber(entrances, ({ house }) => house?.number).map(
+      (entrance) => {
+        const { id, house, reeceState, visitState, comment } = entrance;
+        return (
+          <Fragment key={id}>
+            <Divider />
+            <List.Item
+              onPress={() => onItemPress(entrance)}
+              title={house?.number}
+              description={comment}
+              right={() => (
+                <>
+                  {renderStateListIcon(reeceState)}
+                  {renderStateListIcon(visitState)}
+                </>
+              )}
+            />
+          </Fragment>
+        );
+      }
+    )}
+  </List.Section>
 );
 
 export default StreetEntrancesGroup;
